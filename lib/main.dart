59202b6b8bd86d4e493096aca41be1ba7257be09
miguel silva <megamaante123@gmail.com>
2022-07-23 12:57:34 -0300

    23/07

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/componentes/campo_pesquisa.dart';
+import 'package:flutter_application_1/componentes/card_frete_gratis.dart';
+import 'package:flutter_application_1/componentes/card_assinatura.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,25 +14,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Mercado Aberto',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-
-
-        
-        // scaffoldBackgroundColor: Colors.black  \\mudar cor de fundo da pagina
       ),
-      
-
       home: const MyHomePage(title: 'Mercado Aberto'),
-      
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
@@ -37,71 +34,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xfff5f5f5),  //Mudar a cor do Aplicativo
       appBar: AppBar(
+        toolbarHeight: 86,
         elevation: 0,
-        leading: Icon(Icons.menu,color: Colors.black),  //Icone que vem antes do texto na AppBar
-
-        backgroundColor: Color(0xfff5d415),  //cor da AppBar
-
-        title: Text(widget.title,style: TextStyle(color: Colors.black),),
-       
-       
- actions:[
-   
-   Padding(padding: EdgeInsets.fromLTRB(0, 0, 12, 0),
-   child: Icon(Icons.shopping_cart_outlined,color: Colors.black),  //icone que vem depois do texto na AppBar
-   ),
- ],
-
-
-     bottom: PreferredSize(
-     preferredSize: Size.fromHeight(50),
-     child: Row(
-       children: [
-         Padding(padding: EdgeInsets.fromLTRB(20, 10, 40, 5),
-         child: Icon(Icons.pin_drop, size: 30),   //Icone antes do sub-texto da AppBar
-         ),
-         Text("Enviar para Mercado Aberto",style: TextStyle(fontSize: 20),),  //sub-texto da AppBar
-       ],
-     ),
-
-     ),
-
-
-      ),
-      body: Container(  // criar um container
-
-        height: 180,  // definir o tamanho do container
-
-        decoration: BoxDecoration(  //decoração da box(container)
-          gradient: LinearGradient(  //cor gradiante
-            begin: Alignment.topCenter,  //definir o inicio da gradiante
-            end: Alignment.bottomCenter,  //definir o final da gradiante
-        colors:[  //cores da gradiante
-           Color(0xfff5d415),
-           Color(0xfff5f5f5),
-        ]
+        backgroundColor:const Color(0xffffdb15),
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: IconButton(
+            onPressed: () => Scaffold.of(context).openDrawer(),
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+              size: 32,
+            ),
+          ),
         ),
+        actions: const [
+          CampoPesquisa(),
+          Padding(padding : EdgeInsets.all(6)),
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.black,
+              size: 32,
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(6)),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(
+            color:const Color (0xffffdb15),
+            height: 40.0,
+            child: Row(
+              children: const [
+                Padding(padding: EdgeInsets.all(5)),
+                Icon(
+                  Icons.pin_drop_outlined,
+                  size: 22,
+                ),
+                Padding(padding: EdgeInsets.all(5)),
+                Text("Enviar para"),
+              ],
+            ),
+          ),
         ),
       ),
+      body: Container(
+        padding: const EdgeInsets.only(
+          left: 15,
+          right: 15,
+        ),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: FractionalOffset(0.5, 0.2),
+            colors: [
+              Color(0xffffdb15),
+              Color(0xfff5f5f5),
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            const Padding(padding: EdgeInsets.all(5)),
+            Container(
+              padding: const EdgeInsets.all(3),
+              height: 180,
+              child: Image.network(
+                'https://http2.mlstatic.com/D_NQ_NP650326-MLA50239834698_062022-B.jpg',
+              ),
+            ),
+              const Padding(padding: EdgeInsets.all(5)),       
+              const CardAssinatura(
+                titulo: "Assine o nivel 6 por R\$ 9,90/mês",
+              ),
+              const Padding(padding: EdgeInsets.all(5)),       
+              const CardFreteGratis(),
+              const Padding(padding: EdgeInsets.all(10)),
+            
+            
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
